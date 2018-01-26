da2b4acd83c9c31efcfa887780ce828319846a91
defindal <defindal@gmail.com>
2018-01-26 08:55:11 +0700

    test merge

--- src/model/Ranker.java
+++ src/model/Ranker.java
@@ -1,4 +1,138 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import entity.Card;
+import java.util.List;
+
+/**
+ *
+ * @author deph
+ */
+public class Ranker {
+    private List<Card> cards;
+    
+    public Ranker(List<Card> cards){
+        if(cards.size() == 5) this.cards = cards;
+    }
+    
+    /**
+     * 
+     * @return kartu as
+     */
+    public boolean isOnePair(){
+        
+        return false;
+    }
+    
+    public boolean isTwoPairs(){
+        
+        return false;
+    }
+    
+    public boolean isThreeAKind(){
+    
+        return false;
+    }
+    
+    public boolean isStraight(){
+        return false;
+    }
+    
+    public boolean isFlush(){
+        
+        return false;
+    }
+     
+      public boolean isFullHouse(){
+        return false;
+    }
+    
+    public boolean isFourOfAKind(){
+        return false;
+    }
+    
+    public boolean isStraighFlush(){
+        return false;
+    }
+    
+    public boolean isRoyalFlush(){
+        return false;
+    }
+}
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import entity.Card;
+import java.util.List;
+
+/**
+ *
+ * @author deph
+ */
+public class Ranker {
+    private List<Card> cards;
+    
+    public Ranker(List<Card> cards){
+        if(cards.size() == 5) this.cards = cards;
+    }
+    
+    /**
+     * 
+     * @return kartu as
+     */
+    public boolean isOnePair(){
+        
+        return false;
+    }
+    
+    public boolean isTwoPairs(){
+        
+        return false;
+    }
+    
+    public boolean isThreeAKind(){
+    
+        return false;
+    }
+    
+    public boolean isStraight(){
+        return false;
+    }
+    
+     public boolean isFlush(){
+        return false;
+    }
+     
+      public boolean isFullHouse(){
+        return false;
+    }
+    
+    public boolean isFourOfAKind(){
+        return false;
+    }
+    
+    public boolean isStraighFlush(){
+        return false;
+    }
+    
+    public boolean isRoyalFlush(){
+        return false;
+    }
+}
+>>>>>>> 9dac317f2d81acf6e217837d980a09a35c0ce795
+=======
+<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -90,14 +224,14 @@ public class Ranker {
         //Hahahaha
         //kimochi
     }
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
+//<<<<<<< HEAD
+//
+//=======
+//<<<<<<< HEAD
     /*
         wuushhh
     */
-=======
+//=======
     /**
 <<<<<<< HEAD
      * Dony Salman Adi Pradana
@@ -110,8 +244,8 @@ public class Ranker {
      * @return awaww
 >>>>>>> c8e98f6a13b0597264c0c3117a1a847d10823b8f
      */
->>>>>>> 8cd39e165f4c0a8432a7e2f7bcb2de0989adb150
->>>>>>> c014b98910c49cd832ff839b256a3474c6a5affb
+//>>>>>>> 8cd39e165f4c0a8432a7e2f7bcb2de0989adb150
+//>>>>>>> c014b98910c49cd832ff839b256a3474c6a5affb
     public boolean isOnePair(){
         
         return false;
@@ -174,17 +308,25 @@ public class Ranker {
     public boolean isRoyalFlush(){
         return false;
     }
-<<<<<<< HEAD
-    //ryan
-=======
-<<<<<<< HEAD
-    
-    /*
-        wuushhh
-    */
-=======
-//    oleh sigit tai
->>>>>>> 8cd39e165f4c0a8432a7e2f7bcb2de0989adb150
->>>>>>> c014b98910c49cd832ff839b256a3474c6a5affb
 }
+<<<<<<< HEAD
+//<<<<<<< HEAD
+//    //ryan
+//=======
+//<<<<<<< HEAD
+//    
+//    /*
+//        wuushhh
+//    */
+//=======
+////    oleh sigit tai
+//>>>>>>> 8cd39e165f4c0a8432a7e2f7bcb2de0989adb150
+//>>>>>>> c014b98910c49cd832ff839b256a3474c6a5affb
+//}
+=======
 >>>>>>> b18497826c45d0a871bf632c9a1e0bfecafa05ae
+>>>>>>> be3343308526f60c2d46351b3f35f0ee0d2c345b
+<<<<<<< HEAD
+>>>>>>> 9dac317f2d81acf6e217837d980a09a35c0ce795
+=======
+>>>>>>> 9dac317f2d81acf6e217837d980a09a35c0ce795
