18efcac62b2116d99915e6faab97a2504193bfe2
Muhammad Donny Setya <35796803+cingcila@users.noreply.github.com>
2018-01-26 10:14:57 +0700

    Awal

--- src/model/Ranker.java
+++ src/model/Ranker.java
@@ -8,115 +8,54 @@ package model;
 import entity.Card;
 import java.util.List;
 
-/**
- *
+/*
  * @author deph
- * @viewer YusufHA, dkk.
  */
 public class Ranker {
-    /**
-     * Komen;
-     */
-    private List<Card> cards;
-    
-    public Ranker(List<Card> cards){
-        if(cards.size() == 5) this.cards = cards;
-        //Hahahaha
-    }
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-    /*
-        wuushhh
-    */
-=======
-    /**
-<<<<<<< HEAD
-     * Dony Salman Adi Pradana
-     * @return 
-=======
-     * 
-<<<<<<< HEAD
-     * @return 
-=======
-     * @return awaww
->>>>>>> c8e98f6a13b0597264c0c3117a1a847d10823b8f
-     */
->>>>>>> 8cd39e165f4c0a8432a7e2f7bcb2de0989adb150
->>>>>>> c014b98910c49cd832ff839b256a3474c6a5affb
-    public boolean isOnePair(){
+    private List<Card> cards;
+
+    public Ranker(List<Card> cards) {
+        if (cards.size() == 5) {
+            this.cards = cards;
+        }
+    }
+
+    public boolean isOnePair() {
+        return false;
+    }
+
+    public boolean isTwoPairs() {
+        return false;
+    }
+
+    public boolean isThreeAKind() {
+        return false;
+    }
+
+    public boolean isStraight() {
         
         return false;
     }
-    
-    /*
-        wuushhh
-    */
-    public boolean isTwoPairs(){
+
+    public boolean isFlush() {
+        return false;
+    }
+
+    public boolean isFullHouse() {
+        return false;
+    }
+
+    public boolean isFourOfAKind() {
+        return false;
+    }
+
+    public boolean isStraighFlush() {
         
         return false;
     }
-    
-    /*
-        wuushhh
-    */
-    public boolean isThreeAKind(){
-    
+
+    public boolean isRoyalFlush() {
         return false;
     }
-    
-    /*
-        wuushhh
-    */
-    public boolean isStraight(){
-        return false;
-    }
-    
-    /*
-        wuushhh
-    */
-     public boolean isFlush(){
-        return false;
-    }
-     
-    /*
-        wuushhh
-    */
-      public boolean isFullHouse(){
-        return false;
-    }
-    
-    /*
-        wuushhh
-    */
-    public boolean isFourOfAKind(){
-        return false;
-    }
-    
-    /*
-        wuushhh
-    */
-    public boolean isStraighFlush(){
-        return false;
-    }
-    
-    /*
-        wuushhh
-    */
-    public boolean isRoyalFlush(){
-        return false;
-    }
-<<<<<<< HEAD
-    //ryan
-=======
-<<<<<<< HEAD
-    
-    /*
-        wuushhh
-    */
-=======
-//    oleh sigit tai
->>>>>>> 8cd39e165f4c0a8432a7e2f7bcb2de0989adb150
->>>>>>> c014b98910c49cd832ff839b256a3474c6a5affb
 }
